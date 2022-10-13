5c5591e355fcb026073c78e1ba835c1745dc1027
Hoang Trung <hoangtrung1801.2003@gmail.com>
2022-10-13 17:32:15 +0700

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,62 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint/eslint-plugin',
+        'unicorn',
+        '@darraghor/nestjs-typed',
+    ],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'plugin:unicorn/recommended',
+        'plugin:@darraghor/nestjs-typed/recommended',
+        'plugin:@darraghor/nestjs-typed/no-swagger',
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+
+        'unicorn/filename-case': [
+            'error',
+            {
+                cases: {
+                    kebabCase: true,
+                    pascalCase: true,
+                },
+            },
+        ],
+        'unicorn/prefer-top-level-await': 'off',
+        'unicorn/prevent-abbreviations': 'off',
+        'unicorn/no-null': 'off',
+        'unicorn/no-static-only-class': 'off',
+        'unicorn/prefer-module': 'off',
+        'unicorn/prefer-node-protocol': 'off',
+
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                trailingComma: 'all',
+                tabWidth: 4,
+                bracketSpacing: true,
+                endOfLine: 'auto',
+            },
+        ],
+
+        '@darraghor/nestjs-typed/api-property-returning-array-should-set-array':
+            'off',
+    },
 };
